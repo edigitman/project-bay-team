9115824f83f163c67c34d757b92aa4671cbfd881
gosav.ioana@gmail.com <gosav.ioana@gmail.com@a2055df6-cf5b-cbe4-9f12-6951b2e79204>
2013-11-18 14:57:32 +0000

    stub of AbstractDAO ? to be checked!

--- project-bay-team/projectBay/src/main/java/ro/ig/projectBay/dao/impl/AbstractDAOImpl.java
+++ project-bay-team/projectBay/src/main/java/ro/ig/projectBay/dao/impl/AbstractDAOImpl.java
@@ -1,7 +1,13 @@
 package ro.ig.projectBay.dao.impl;
 
+import java.util.Collection;
+
 import ro.ig.projectBay.dao.AbstractDAO;
 
 public class AbstractDAOImpl <T> implements AbstractDAO <T> {
 
+	public void add(T object){}
+	public Collection<T> getAll(){ return null;}
+	public void edit(T initialObject, T newObject){}
+	public void delete(T object){}
 }
