6fe0ddeea1f4bd7d38b1f51912e996d672bbea9d
gosav.ioana@gmail.com <gosav.ioana@gmail.com@a2055df6-cf5b-cbe4-9f12-6951b2e79204>
2013-12-23 13:38:14 +0000

    Made it work, god damn it!

--- trunk/project-bay-team/projectBay/src/main/java/ro/ig/projectBay/web/LocaleMB.java
+++ trunk/project-bay-team/projectBay/src/main/java/ro/ig/projectBay/web/LocaleMB.java
@@ -3,19 +3,25 @@ package ro.ig.projectBay.web;
 import java.io.Serializable;
 import java.util.Locale;
 
-import javax.enterprise.inject.Produces;
-import javax.faces.bean.SessionScoped;
+import javax.annotation.PostConstruct;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
+import org.springframework.context.annotation.Scope;
+
 @Named
-@SessionScoped
+@Scope("session")
 public class LocaleMB implements Serializable{
 
 	private static final long serialVersionUID = 905375762230221180L;
 	
-	@Produces
-    private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+	private Locale locale;
+	
+	@PostConstruct
+	public void init(){
+		locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();	
+	}
 
     // ======================================
     // =          Business methods          =
