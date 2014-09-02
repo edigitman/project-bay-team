9f2e163b53a81a86cfb13684fde63b42d8f24b90
edi.gitman@gmail.com <edi.gitman@gmail.com@a2055df6-cf5b-cbe4-9f12-6951b2e79204>
2014-09-02 11:26:04 +0000

    Added fetch type on UserRoles, http://www.javacodegeeks.com/2012/07/four-solutions-to-lazyinitializationexc_05.html

--- trunk/project-bay-team/projectBay/src/main/java/ro/ig/projectBay/model/User.java
+++ trunk/project-bay-team/projectBay/src/main/java/ro/ig/projectBay/model/User.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -51,7 +52,7 @@ public class User {
 	 * @OneToMany(mappedBy = "user") private List<UserRole> userRoles;
 	 */
 
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "UserRole", joinColumns = @JoinColumn(name = "id_user"), inverseJoinColumns = @JoinColumn(name = "id_role"))
 	List<Role> roleList;
 
