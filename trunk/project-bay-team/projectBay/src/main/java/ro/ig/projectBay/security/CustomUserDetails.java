af05d71ae88e88953f4dc4d8e3372d6bb6ecff51
edi.gitman@gmail.com <edi.gitman@gmail.com@a2055df6-cf5b-cbe4-9f12-6951b2e79204>
2014-09-02 10:02:52 +0000

    Code clean-up, replaced deprecated.

--- trunk/project-bay-team/projectBay/src/main/java/ro/ig/projectBay/security/CustomUserDetails.java
+++ trunk/project-bay-team/projectBay/src/main/java/ro/ig/projectBay/security/CustomUserDetails.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -29,14 +29,13 @@ public class CustomUserDetails implements UserDetails {
 		this.user = user;
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public Collection<GrantedAuthority> getAuthorities() {
 
 		Collection<GrantedAuthority> auth = new ArrayList<>();
 		
 	for (Role userRole : user.getRoleList()) {
-			auth.add(new GrantedAuthorityImpl("ROLE_" + userRole.getCode()));
+			auth.add(new SimpleGrantedAuthority("ROLE_" + userRole.getCode()));
 		}
 
 		return auth;
