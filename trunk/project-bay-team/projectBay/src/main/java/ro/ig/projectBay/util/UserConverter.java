d4c3c18e18987b02f799d061ba311fc46e4af1fa
edi.gitman@gmail.com <edi.gitman@gmail.com@a2055df6-cf5b-cbe4-9f12-6951b2e79204>
2014-09-08 15:02:00 +0000

    Added converter.

---
+++ trunk/project-bay-team/projectBay/src/main/java/ro/ig/projectBay/util/UserConverter.java
@@ -0,0 +1,34 @@
+package ro.ig.projectBay.util;
+
+import javax.faces.bean.ManagedProperty;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import ro.ig.projectBay.model.User;
+import ro.ig.projectBay.service.UserService;
+
+@FacesConverter(forClass = User.class, value = "userConverter")
+public class UserConverter implements Converter {
+	
+	@ManagedProperty(value = "#{userService}")
+	private UserService userService;
+	
+	@Override
+	public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) {
+		
+		return userService.findByLogin(arg2);
+	}
+
+	@Override
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) {
+
+		if (arg2 instanceof User) {
+			User u = (User) arg2;
+			return u.getLogin();
+		}
+		return null;
+	}
+
+}
